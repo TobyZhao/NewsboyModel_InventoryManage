050af1fef5f26c9ebc0e1ec6dff96a901330fdad
Toby Tianxiao Zhao <tzh@kth.se>
2016-12-05 21:49:33 +0100

    Update main.cpp

--- C-code/main.cpp
+++ C-code/main.cpp
@@ -2,40 +2,39 @@
 #include "init.cpp"
 int main()
 {
-	int i,flag=0;
-	double a,sum=0;
-//  可以人工输入各项参数，为调试方便，各参数在头文件中设置好了
+	int i, flag = 0;
+	double a, sum = 0;
+//  manually input parameters
 //	printf("Enter Initial Inventory:\n");
-//	scanf("%d",initil); //初始库存量
+//	scanf("%d",initil);	// initial inventory
 //	printf("Enter Simulation Months:\n");
-//	scanf("%d",nmnths);	//仿真运行时间
+//	scanf("%d",nmnths);	// simulation time
 //	printf("Enter number of policies:\n");
-//	scanf("%d",npolcy);	//策略数
+//	scanf("%d",npolcy);	// number of policies
 //	printf("Enter number of demand sizes:\n");
-//	scanf("%d",nvalue);	//需求量
+//	scanf("%d",nvalue);	// demands
 //	printf("Enter mean Interdemand Time:\n");
-//	scanf("%d",mdemdt);	//平均需求间隔时间
+//	scanf("%d",mdemdt);	// mean interdemand time
 //	printf("Enter cost of item to prepaer:\n");
-//	scanf("%d",setupc);	//订货附加费
+//	scanf("%d",setupc);	// setup cost
 //	printf("Enter order cost per item:\n");
-//	scanf("%d",incrmc);	//每件订货费
+//	scanf("%d",incrmc);	// order cost per item
 //	printf("Enter mean maitaining cost:\n");
-//	scanf("%d",H);		//每件货每月保管费
+//	scanf("%d",H);		// mean maitaining cost
 //	printf("Enter mean cost per item for loss:\n");
-//	scanf("%d",p1);		//每件货损失费
+//	scanf("%d",p1);		// mean cost per item for loss
 ////	printf("Enter distributionof demand sizes:\n");
 ////	scanf();
 
-
-	for(i=1;i<=npolcy;i++)
+	for(i = 1; i <= npolcy; i++)
 	{
 		printf("Enter smalls and bigs:\n");
-		scanf("%d%d",&smalls,&bigs);
-		flag=0;
+		scanf("%d%d", &smalls, &bigs);
+		flag = 0;
 		init();
 		while(1)
 		{
-			if(flag==1)
+			if(flag == 1)
 				break;
 			timing();
 			switch(next)
@@ -44,16 +43,14 @@ int main()
 				break;
 			case 2:	demand();
 				break;
-			case 3:	report(i);flag=1;
+			case 3:	report(i);flag = 1;
 				break;
 			case 4:	evalus();
 				break;
-			default:	printf("default error");
+			default: printf("default error");
 				break;
 			}
 		}
-
 	}
-
 	return 0;
 }
